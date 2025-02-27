d3d09f07ecb818a729d65c0ffdbb82636f51f0a2
Pedro Henrique Ribeiro <127356379+class-pedro@users.noreply.github.com>
2025-02-26 21:56:12 -0300

    feat: add root modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ModulesModule } from './modules/modules.module';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './shared/prisma/prisma.module';
+import { WalletsModule } from './modules/wallets/wallets.module';
+import { CreditCardsModule } from './modules/credit-cards/credit-cards.module';
+import { DebitCardsModule } from './modules/debit-cards/debit-cards.module';
+import { ExpensesModule } from './modules/expenses/expenses.module';
+import { AmountsModule } from './modules/amounts/amounts.module';
+import { CardsModule } from './modules/cards/cards.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ModulesModule],
+  imports: [PrismaModule, UsersModule, WalletsModule, CreditCardsModule, DebitCardsModule, ExpensesModule, AmountsModule, CardsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
